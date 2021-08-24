e4d60d894a9fd36ab64259ad9685f6d804476c26
eduardf <eduardf@bit-it.ru>
2021-08-24 14:48:37 +0300

    Test byFoma

--- app/src/main/java/com/pushe/worker/data/SummaryFragment.kt
+++ app/src/main/java/com/pushe/worker/data/SummaryFragment.kt
@@ -4,28 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.pushe.worker.operations.OperationActivity
-
 import com.pushe.worker.operations.model.OperationDataSource
-import com.pushe.worker.operations.model.Operation
 
 class SummaryFragment : Fragment() {
 
-    private var _binding: FragmentSummaryBinding? = null
-    private val binding get() = _binding!!
-
-    private var operation: Operation? = null
+    private lateinit var operationDataSource: OperationDataSource;
 
     private val args: SummaryFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val operationDataSource = OperationDataSource(requireContext())
+        operationDataSource = OperationDataSource(requireContext())
         operationDataSource.requestOperation(args.barcode)
-        operationDataSource.observe(this, resultObserver)
 
     }
 
@@ -33,40 +29,18 @@ class SummaryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentSummaryBinding.inflate(inflater, container, false)
-        binding.userName.text = "ФИО"
-        binding.result.setOnClickListener { _ -> findNavController().navigate(
-            SummaryFragmentDirections.actionSummaryToList(userId = args.userId)) }
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private val resultObserver: Observer<Result<*>> = Observer { result ->
-
-        if (result is Success<*>) {
-            operation = result.data as Operation
-            operation!!.let {
-                binding.operationName.text = it.name
-                binding.operationDate.text = it.date
-//                binding.operationTime.text = it.duration.toString()
-//                binding.operationSum.text = it.rate.toString() }
-                binding.operationTime.text = it.amount.toString()
-                binding.operationSum.text = it.tarrif.toString() }
-
-            binding.loading.visibility = View.GONE
-            binding.result.setBackgroundResource(R.drawable.ic_bc_processing_result_ok)
-            binding.result.visibility = View.VISIBLE
-        } else {
-            binding.loading.visibility = View.GONE
-            binding.result.setBackgroundResource(R.drawable.ic_bc_processing_result_closed)
-            binding.result.visibility = View.VISIBLE
-        }
+        return ComposeView(requireContext()).apply { setContent { ResultScreen(operationDataSource) } }
 
     }
 
+
+    @Composable
+    fun ResultScreen(operationDataSource: OperationDataSource) {
+
+        Text("Hello!!!")
+//        val result by operationDataSource.observeAsState()
+//
+//        ShowOperationResult(result = result as Result<Any>?)
+
+    }
 }
--- app/src/main/java/com/pushe/worker/operations/model/OperationDataSource.kt
+++ app/src/main/java/com/pushe/worker/operations/model/OperationDataSource.kt
@@ -50,7 +50,4 @@ class OperationDataSource(val context: Context) : MutableLiveData<Result<*>>() {
         }
     }
 
-    fun observeAsState(): Any {
-
-    }
-}
+ }
