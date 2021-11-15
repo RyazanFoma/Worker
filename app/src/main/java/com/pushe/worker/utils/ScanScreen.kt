4edb87af4041304347f48947747e24dc1d68f2b7
eduardf <eduardf@bit-it.ru>
2021-11-15 21:46:34 +0300

    Timeout implemented in ScanScreen.kt

--- app/src/main/java/com/pushe/worker/utils/ScanScreen.kt
+++ app/src/main/java/com/pushe/worker/utils/ScanScreen.kt
@@ -14,38 +14,55 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.google.zxing.client.android.BeepManager
 import com.journeyapps.barcodescanner.CaptureManager
 import com.journeyapps.barcodescanner.CompoundBarcodeView
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScanScreen(statusText: String? = null, scopeAction: (String) -> Unit) {
     val context = LocalContext.current
     var scanFlag by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
     val compoundBarcodeView = remember {
         CompoundBarcodeView(context).apply {
-
             val capture = CaptureManager(context as Activity, this)
             val beepManager = BeepManager(context)
+
             capture.initializeFromIntent(context.intent, null)
-            statusText?.let { this.setStatusText(it) }
             context.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
             capture.decode()
+
+            this.resume()
             this.decodeContinuous { result ->
                 if(scanFlag){
                     return@decodeContinuous
                 }
                 scanFlag = true
-                result.text?.let { barCodeOrQr->
+                result.text?.let { barCode ->
                     if (!context.isDestroyed) {
-                        Log.i("ScanScreen", "BarCode $barCodeOrQr") /* TODO: Remove Log.i */
+                        Log.i("ScanScreen", "BarCode $barCode") /* TODO: Remove Log.i */
                         beepManager.playBeepSoundAndVibrate()
-                        context.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
-                        scopeAction(barCodeOrQr)
+                        scopeAction(barCode)
                     }
+                    scanFlag = false
                 }
-                scanFlag = false
+                scope.cancel()
+                context.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
                 capture.onPause()
                 capture.onDestroy()
             }
-            this.resume()
+
+            scope.launch {
+                repeat(16) { times ->
+                    this@apply.setStatusText((statusText?:"Осталось") + " - ${15 - times}")
+                    delay(1_000)
+                }
+                this@apply.barcodeView.stopDecoding()
+                capture.onPause()
+                capture.onDestroy()
+                context.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
+                context.onBackPressed()
+            }
         }
     }
 
